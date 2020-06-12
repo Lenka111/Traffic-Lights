9abe586a2f22ad2856f2ad2b8bf67a3158dd32bc
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-06-12 13:52:10 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
+//Elena Voinu
 public class Main {
     public static void main(String[] args) {
 
-        //enum instance array
+        //Create the enum instance array
         //assign the constants of the enum to this array
-        //values() to get an array of enum values
+        //use values() method to get an array of enum values
         TrafficLight [] lights = TrafficLight.values();
 
         //iterate the array
